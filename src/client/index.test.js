9eb87f6c0652468ceb0811ee7dea98c75fa3d55d
Mario Medrano Maldonado <mariomedranomaldonado@gmail.com>
2019-03-06 16:32:03 -0400

    Test file added.

--- src/client/index.test.js
+++ src/client/index.test.js
@@ -1,10 +1,2 @@
 // import React from 'react';
 // import renderer from 'react-test-renderer';
-// import ChessBoard from './target';
-
-// describe('Main Feature', () => {
-//    it('renders Good', () => {
-//       const component = renderer.create(<ChessBoard />);
-//       expect(component.toJSON).toMatchSnapshot();
-//    });
-// });
